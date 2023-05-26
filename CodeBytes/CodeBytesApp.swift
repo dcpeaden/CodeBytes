b032c7c72b0076588dc0a9a88a6c7d4bf13b4d8e
Cole Peaden <dcpeaden@gmail.com>
2023-05-26 09:33:13 -0500

    Initial Commit

---
+++ CodeBytes/CodeBytesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeBytesApp.swift
+//  CodeBytes
+//
+//  Created by Cole Peaden on 5/26/23.
+//
+
+import SwiftUI
+
+@main
+struct CodeBytesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
